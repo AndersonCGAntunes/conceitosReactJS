0872cae824e63be362f96f497627c3f967be9111
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2022-05-11 16:22:01 -0300

    Renderização Condicional

--- funcionalidade_app/src/App.js
+++ funcionalidade_app/src/App.js
@@ -1,9 +1,47 @@
 import './App.css';
 
-function App() {
+const buttonA = <button>Histórico dos clientes</button>
+const buttonB = <button>Cadastrar Cliente</button>
+const customer = 'Anderson Clayton'
+
+const hasCostumer = true
+
+const App = () => {
+
+  const renderShowHistory = () => (
+    <div>
+      Clique no botão abaixo para visualizar o histórico dos clientes
+      <br/>
+      {buttonA}
+    </div>
+  )
+
+  const renderAddCustomer = () => (
+    <div>
+      Clique abaixo para cadastrar o cliente
+      <br/>
+      {buttonB}
+    </div>
+  )
+
+  const showCustomer = () => {
+    if (!hasCostumer) return null
+
+    return (
+      <div>
+        <h1>Nome do cliente: Anderson Clayton</h1>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <h2>Olá, mundo.</h2>
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Bem vindo à nossa aula =D.</p>
+      {hasCostumer ? renderShowHistory() : renderAddCustomer()}
+      <div>
+        {showCustomer()}
+      </div>
     </div>
   );
 }
